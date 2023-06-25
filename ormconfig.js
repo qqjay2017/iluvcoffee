96c754e46c6faecc246b064b547d10049a7ca2eb
huangbo <qqjay2013@gmail.com>
2023-06-25 16:28:13 +0800

    dependency inject

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'testDB',
+  autoLoadEntities: true,
+  synchronize: true,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
